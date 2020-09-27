464cbfd251705d36ca3c96acd9579dfee633b2f9
andreasoda <andrea.soda@outlook.it>
2020-09-27 20:00:38 +0200

    Finito login con Facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,6 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, '3761681217175433', '2b61b72e6865c0b8520140a211af59a1'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
