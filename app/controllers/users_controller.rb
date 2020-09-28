0898f32eee2b34d0b8f1bb1343e009e426c7b0f4
andreasoda <andrea.soda@outlook.it>
2020-09-28 19:34:31 +0200

    init favorites

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  def index
+    
+  end
+  
+  def show
+    @current_user = User.all.where(["id LIKE ?", "#{current_user.id}"]).first
+  end
+  
+end
