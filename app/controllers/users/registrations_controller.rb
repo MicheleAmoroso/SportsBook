b4bec4e072826b8549bd9f92ad918b18be898164
andreasoda <andrea.soda@outlook.it>
2020-09-30 17:08:48 +0200

    custom devise e gestione eliminazione utenti

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,22 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   
+  def destroy
+    @reviews = Review.all.where(:user_id => current_user.id)
+    @reviews.each do |review|
+      @ground = Ground.find(review.ground_id)
+      @ground.ratingNum = @ground.ratingNum - 1
+      @ground.ratingSum = @ground.ratingSum - review.rating
+      @ground.save!
+    end
+    @reviews.destroy_all
+
+    @books = Book.all.where(:user_id => current_user.id)
+    @books.each do |book|
+      book.user_id = nil
+      book.save!
+    end
+
+    super
+  end
+  
 end
