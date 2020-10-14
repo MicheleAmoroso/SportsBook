062cccf7067107e45be213eb7ea2d9d409154222
MicheleAmoroso <michele.amoroso1997@gmail.com>
2020-10-14 10:39:44 +0200

    rspec model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,10 @@
 class Review < ActiveRecord::Base
   resourcify
+
+  validates :rating, presence: true  
+  validates :comments, presence: true
+  validates :user_id, presence: true
+  validates :ground_id, presence: true
   
   belongs_to :ground
   belongs_to :user
---
+++ spec/models/review_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe Review, type: :model do
+
+  before(:all) do
+    @user = FactoryBot.create(:user)
+    @ground = FactoryBot.create(:ground)
+  end
+
+  after(:all) do
+    @user.destroy
+    @ground.destroy
+  end
+
+  describe "Creating a review" do
+    it "should be permitted" do
+      review = Review.new(rating: 4, comments: "Bel campo", user_id: @user.id, ground_id: @ground.id).save
+      expect(review).to eq(true)
+    end
+
+    it "is not valid without the rating" do
+      review = Review.new(comments: "Bel campo", user_id: @user.id, ground_id: @ground.id).save
+      expect(review).to eq(false)
+    end
+
+    it "is not valid without the comments" do
+      review = Review.new(rating: 4, user_id: @user.id, ground_id: @ground.id).save
+      expect(review).to eq(false)
+    end
+
+    it "is not valid without the user" do
+      review = Review.new(rating: 4, comments: "Bel campo", ground_id: @ground.id).save
+      expect(review).to eq(false)
+    end
+
+    it "is not valid without the ground" do
+      review = Review.new(rating: 4, comments: "Bel campo", user_id: @user.id).save
+      expect(review).to eq(false)
+    end
+    
+  end
+
+  
+  
+end
